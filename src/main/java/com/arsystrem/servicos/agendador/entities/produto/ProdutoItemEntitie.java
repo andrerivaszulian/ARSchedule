b0667b9a502b1a50bfd60a6ba1e14c9f00c30e91
andre.zulian <andre.zulian@trabalhandocomakaspper.com.br>
2024-04-17 11:28:22 -0300

    i

--- src/main/java/com/arsystrem/servicos/agendador/entities/produto/ProdutoItemEntitie.java
+++ src/main/java/com/arsystrem/servicos/agendador/entities/produto/ProdutoItemEntitie.java
@@ -1,4 +1,25 @@
 package com.arsystrem.servicos.agendador.entities.produto;
 
-public class ProdutoItemEntitie {
+import com.arsystrem.servicos.agendador.commons.base.entity.BaseEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "produto_item")
+public class ProdutoItemEntitie extends BaseEntity {
+    @ManyToOne
+    @JoinColumn(name = "fk_produto")
+    private ProdutoEntitie produto;
+
+    @ManyToOne
+    @JoinColumn(name = "fk_produto_item")
+    private ProdutoEntitie produtoItem;
 }
+
